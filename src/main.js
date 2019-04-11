949a34c3c67a3b20d22db006494ccb46f4291602
greg <gmrempe@gmail.com>
2019-04-11 11:31:53 -0400

    Setup initial files

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasEl = document.getElementsByTagName("canvas")[0];
+    canvasEl.width = 500;
+    canvasEl.height = 500;
+
+    const ctx = canvasEl.getContext("2d");
+});
