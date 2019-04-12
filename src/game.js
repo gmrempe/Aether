13a2a9b3f443c753672f5d0a48eb966160c56963
greg <gmrempe@gmail.com>
2019-04-12 15:00:43 -0400

    Randomly generate map

--- src/game.js
+++ src/game.js
@@ -4,31 +4,83 @@ import CanvasState from "./canvasState";
 import Player from "./player";
 
 class Game {
-    constructor(canvasEl, ctx) {
+    constructor(canvasEl, ctx, playerCount) {
         this.ctx = ctx;
         this.canvas = canvasEl;
+        this.playerCount = playerCount;
         this.player = new Player('#0095DD', this.ctx);
         this.canvasState = new CanvasState(this.canvas, this.player);
-        this.base1 = new Base(500, 300, 30, '#0095DD', this.ctx);
-        this.base2 = new Base(75, 75, 30, '#ff0000', this.ctx);
-        this.base3 = new Base(280, 200, 30, '#ffffff', this.ctx);
-
-        this.canvasState.addShape(this.base1);
-        this.canvasState.addShape(this.base2);
-        this.canvasState.addShape(this.base3);
+        this.bases = [];
+        // this.base1 = new Base(500, 300, 30, '#0095DD', this.ctx);
+        // this.base2 = new Base(75, 75, 30, '#ff0000', this.ctx);
+        // this.base3 = new Base(280, 200, 30, '#ffffff', this.ctx);
+        this.baseGenerator();
+        this.addShapes();
+        // this.canvasState.addShape(this.base1);
+        // this.canvasState.addShape(this.base2);
+        // this.canvasState.addShape(this.base3);
+    }
+    
+    addShapes() {
+        for (let i = 0; i < this.bases.length; i++) {
+            this.canvasState.addShape(this.bases[i]);
+        }
     }
 
+    baseGenerator() {
+        let num = Math.floor(Math.random() * 10);
+        if (num < 3) {
+            num = 3
+        }
+        for(let i = 0; i < num; i++) {
+            this.bases.push(new Base(this.basePosX(), this.basePosY(), 30, this.colorPicker()))
+        }
+    }
+    
+    basePosX() {
+        let x = Math.random() * 650;
+        if(x <= 30) {
+            x = 30;
+        }
+        for(let i = 0; i < this.bases.length; i++) {
+            while(Math.abs(this.bases[i].posX - x) <= 50) {
+                x = Math.random() * 650;
+            }
+        }
+        return x;
+    }
+    
+    basePosY() {
+        let y = Math.random() * 350
+        if (y <= 30) {
+            y = 30;
+        }
+        for(let i = 0; i < this.bases,length; i++) {
+            while(Math.abs(this.bases[i].posY - y) <= 50) {
+                y = Math.random() * 350;
+            }
+        }
+        return y;
+    }
 
-
-
+    colorPicker() {
+      if(this.bases.length < 1) {
+          return '#0095DD';
+      } else if(this.bases.length < 2) {
+          return '#ff0000';
+      } else {
+          return '#ffffff';
+      }
+    }
 
     drawGame() {
-        // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.base1.draw(this.ctx);
-        this.base2.draw(this.ctx);
-        this.base3.draw(this.ctx);
+        for (let i = 0; i < this.bases.length; i++) {
+            this.bases[i].draw(this.ctx);
+        }
+        // this.base1.draw(this.ctx);
+        // this.base2.draw(this.ctx);
+        // this.base3.draw(this.ctx);
         this.player.playerDraw(this.ctx);
-        // requestAnimationFrame(draw);
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import Game from "./game";
 document.addEventListener("DOMContentLoaded", () => {
     const canvasEl = document.getElementById("canvas");
     const ctx = canvasEl.getContext("2d");
-
+    const playerCount = 2;
     // const bases = [];    //put bases into array and then iterate through to draw
     // bases = () => {
     //    bases.push((new Base(40, 40, '#0095DD')))
     // }
-    const game = new Game(canvasEl, ctx)
+    const game = new Game(canvasEl, ctx, playerCount)
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
