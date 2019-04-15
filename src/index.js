ba7f3a7f4cfd8a98131a803f2d00a1a0b33f7e93
greg <gmrempe@gmail.com>
2019-04-15 17:57:17 -0400

    Opening menu

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import Game from "./game";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const canvasEl = document.getElementById("canvas");
-    const ctx = canvasEl.getContext("2d");
-    const playerCount = 4;
-    const game = new Game(canvasEl, ctx, playerCount)
+    const menu = document.getElementById('ui-wrapper');
 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.drawGame();
-        requestAnimationFrame(draw);
+    function hide(e) {
+        e.target.style.display = 'none';
+        e.currentTarget.parentElement.firstElementChild.style.display = 'none';
+        e.currentTarget.parentElement.style.zIndex = '-1';
+    }
+    
+    function show(e) {
+        e.target.style.display = 'block';
+        e.currentTarget.parentElement.firstElementChild.style.display = 'block';
+        e.currentTarget.parentElement.style.zIndex = '2';
     }
 
-    requestAnimationFrame( () => {
-        draw(ctx);
-    })    
+    document.querySelectorAll('.play')[0].addEventListener('click', function (menu) {
+        hide(menu);
+        startGame(4);
+    });
+
+    function startGame(playerCount = 4) {  
+        const canvasEl = document.getElementById("canvas");
+        const ctx = canvasEl.getContext("2d");
+        const game = new Game(canvasEl, ctx, playerCount);  
+        
+        function draw() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            game.drawGame();
+            requestAnimationFrame(draw);
+        }
+
+        requestAnimationFrame( () => {
+            draw();
+        })
+    }
+    
 });
