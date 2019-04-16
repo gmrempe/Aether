43fefaf1b142c22db16a77286c7e7efa97fe2bd5
greg <gmrempe@gmail.com>
2019-04-16 10:22:08 -0400

    add settings menu

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import Game from "./game";
 import Base from "./base";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById('ui-wrapper');
+    const settings = document.getElementById('settings');
     let canvasEl = document.getElementById("canvas");
     let ctx = canvasEl.getContext("2d");
+    const menu = document.getElementById('ui-wrapper');
     requestAnimationFrame( () => {
-        const b1 = new Base({ x: 70, y: 40 }, 25, "#327f95");
+     const b1 = new Base({ x: 70, y: 40 }, 25, "#327f95");
         const b2 = new Base({ x: 550, y: 40 }, 25, "green");
         const b3 = new Base({ x: 500, y: 100 }, 25, "#d6d9ca");
         const b4 = new Base({x: 550, y: 250}, 25, "purple");
-        const b5 = new Base({x: 170, y: 180}, 25, "red");
+        const b5 = new Base({x: 150, y: 180}, 25, "red");
         b1.draw(ctx);
         b2.draw(ctx);
         b3.draw(ctx);
@@ -18,21 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
         b5.draw(ctx);
     })
     
-    function hide(e) {
-        e.target.style.display = 'none';
-        e.currentTarget.parentElement.firstElementChild.style.display = 'none';
-        e.currentTarget.parentElement.style.zIndex = '-1';
+    function hide(ele) {
+        ele.style.display = 'none';
+        ele.style.zIndex = '-1';
     };
     
-    function show(e) {
-        e.target.style.display = 'block';
-        e.currentTarget.parentElement.firstElementChild.style.display = 'block';
-        e.currentTarget.parentElement.style.zIndex = '2';
+    function show(ele) {
+        ele.style.display = 'block';
     };
     
-    document.querySelectorAll('.play')[0].addEventListener('click', function (menu) {
+    document.querySelectorAll('.play')[0].addEventListener('click', function () {
         hide(menu);
         startGame(4);
+    });
+    
+    document.querySelectorAll('.settings')[0].addEventListener('click', function () {
+        hide(menu);
+        show(settings);
     });
 });
 
